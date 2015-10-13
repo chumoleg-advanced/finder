a57117536610639928613233ca32915bd0b6568f
Chumakov Oleg <chumoleg@yandex.ru>
2015-10-13 23:17:50 +0600

    change views search

--- frontend/views/site/form.php
+++ frontend/views/site/form.php
@@ -10,15 +10,24 @@ $this->title = Yii::t('title', 'Search');
 
 <div class="row" style="margin-top: 20vh;">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12"></div>
-        <div class="col-lg-6 col-lg-offset-3 col-sm-6 col-sm-offset-3">
-            <?= Html::textInput('searchField', null,
-                ['class' => 'col-lg-12 col-md-12 col-sm-12 col-xs-12',
-                 'placeholder' => 'Введите запрос ...']); ?>
-        </div>
+    <div class="col-lg-6 col-lg-offset-3 col-sm-6 col-sm-offset-3">
+        <?= Html::textInput('searchField', null, [
+            'class'       => 'col-lg-12 col-md-12 col-sm-12 col-xs-12',
+            'placeholder' => 'Введите запрос ...'
+        ]);
+        ?>
+    </div>
+    <div class="col-lg-6 col-lg-offset-3 col-sm-6 col-sm-offset-3">
+        <?= Html::textarea('searchField', null, [
+            'class'       => 'col-lg-12 col-md-12 col-sm-12 col-xs-12',
+            'style'       => 'resize: none;',
+            'placeholder' => 'Опишите подробнее ...'
+        ]); ?>
+    </div>
 
-        <div>&nbsp;</div>
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
-            <a class="btn btn-lg btn-success" href="<?= Url::toRoute('/site/result'); ?>">
-                Отправить запрос</a>
-        </div>
+    <div>&nbsp;</div>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+        <a class="btn btn-lg btn-success" href="<?= Url::toRoute('/site/result'); ?>">
+            Отправить запрос</a>
+    </div>
 </div>
