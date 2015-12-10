76e5c81cf9751fe38d1d070b2c951819b4836b6b
chumakov.o <chumakov.o@thor-dev.ru>
2015-12-10 17:35:06 +0600

    change captcha

--- frontend/views/search/_forms/_parts/_captcha.php
+++ frontend/views/search/_forms/_parts/_captcha.php
@@ -6,10 +6,12 @@ use \yii\captcha\Captcha;
 <div class="form-group">
     <div class="col-md-offset-2 col-md-10 col-sm-12 col-xs-12">
         <hr/>
-        <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
+        <?= $form->field($model, 'verifyCode', [
+            'options' => ['class' => 'verifyCodeBlock form-group']
+        ])->widget(Captcha::className(), [
             'options'  => ['class' => 'form-control verifyCodeInput'],
-            'template' => '<div class="col-md-2 col-sm-2 col-xs-6">{image}</div>
-                    <div class="col-md-4 col-sm-4 col-xs-6">{input}</div>',
+            'template' => '<div class="col-md-3 col-sm-4 col-xs-12">{image}</div>
+                    <div class="col-md-4 col-sm-6 col-xs-12">{input}</div>',
         ]); ?>
     </div>
 </div>
