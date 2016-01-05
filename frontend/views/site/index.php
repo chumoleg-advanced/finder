34c7987205b598b5667fac3ca4667049208498f5
chumakov.o <chumakov.o@thor-dev.ru>
2016-01-05 14:44:01 +0600

    cookie city

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -5,14 +5,30 @@
 
 use \yii\helpers\Url;
 use himiklab\thumbnail\EasyThumbnailImage;
+use yii\web\Cookie;
 
 $this->title = Yii::t('title', 'Search');
 ?>
 
 <div class="site-index">
     <div class="col-md-12 text-center">
-        <?php $info = new common\components\GeoIpInfo(); ?>
-        <h3>Ваш город: <?= $info->getValue('city'); ?></h3>
+        <?php
+        $city = Yii::$app->getRequest()->getCookies()->getValue('city');
+
+        if (empty($city)) {
+            $info = new common\components\GeoIpInfo();
+            $city = $info->getValue('city');
+
+            $cookie = new Cookie([
+                'name'   => 'city',
+                'value'  => $city,
+                'expire' => time() + 86400 * 365,
+            ]);
+            Yii::$app->getResponse()->getCookies()->add($cookie);
+        }
+        ?>
+
+        <h3>Ваш город: <?= $city; ?></h3>
     </div>
 
     <div class="col-md-12 text-center">
